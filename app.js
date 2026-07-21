9713fc34db36d585fce4b1782dba3fac24e0cfe0
sebastian07A <juansebastiananguloperez1@gmail.com>
2026-07-21 11:42:06 -0500

    endpoints creativo

--- app.js
+++ app.js
@@ -1,13 +1,34 @@
-//import express from "express";//ES
+import express from "express";//ES
+//mport {configDotenv} from "dotenv"
+//configDotenv()
+import "dotenv/config"
 
-const express = require("express")
+//const express = require("express");
 const app = express();
-const puerto = process.env.POST || 3000;
+const puerto = process.env.PORT || 3500;
 
 app.get("/",(req,res)=>{
-    res.send("Hola ficha 3407180")
+    res.send("Hola ficha 3407180 del SENA")
 });
 
+app.get("/misaludo",(req,res)=>{
+    res.send(`<h1>saludo</h1><p>Hola soy Sebas</p>`)
+});
+
+app.get("/cliente/:id",(req,res)=>{
+    const id = req.params.id
+    res.send(`<h1>Clientes</h1>
+        <p>Soy cliente con ID${id}</p>`)
+});
+
+app.get("/presentacion/:id",(req,res)=>{
+    const id = req.params.id
+    res.send(`<h1>Presentacion</h1>
+        <p>Soy sebastian aprendiz sena con ID${id}</p>`)
+});
+
+
 app.listen(puerto, ()=>{
-    console.log(`Servidor funcional!! en el puerto ${puerto}`);
+    console.log(`SERVIDOR http://localhost: ${puerto}
+        http://127.0.0.1:${puerto}`);
 });
