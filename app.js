06cf3abd11e156f66a0295887ab72e18c4a24d80
santicaro11sep-sketch <santicaro11sep@gmail.com>
2026-08-18 11:50:39 -0500

    endpoints de adicionar y de modificar

--- app.js
+++ app.js
@@ -1,18 +1,83 @@
-const express = require( "express")
-const app = express();
+const { error } = require("console")
+const express = require("express")
+const app = express()
+  //configuracion de body-parse
+app.use(express.json())
+const sistemaArchivo = require("fs")
+const ruta = require("path")
+
 const PORT = process.env.PORT || 3000;
 
-app.get("/",(req,res)=>{
-    res.send(`<h1>Api aprendices</h1>`)
-});
+//ruta de mi archivo json
+const rutaArchivojson = ruta.join(__dirname, "aprendices.json")
 
-//Lista para crear endpoint
-app.get("api/aprendices", (req, res) => {
-    res.json({menssaje: "endpoint para listar aprendices"})
+app.get("/", (req, res) => {
+    res.send(`<h1>Api aprendices</h1>`)
 })
 
+//Lista para crear endpoint
+app.get("/api/aprendices", (req, res) => {
+    sistemaArchivo.readFile(rutaArchivojson, "utf-8", (error, datos) => {
+        if (error) {
+            return res.status(500).json({ Error: "Error conexion bd." })
+        }
+        const listaAprendices = JSON.parse(datos)
+        res.json(listaAprendices)
+    })
+})
 
-app.listen(PORT, ()=>{
+//endpoint para adicionar
+app.post("/api/aprendices", (req, res) => {
+    //capturar los datos enviados
+    const datosAprendiz = req.body
+    sistemaArchivo.readFile(rutaArchivojson, "utf-8", (error, datos) => {
+        if (error) {
+            return res.status(500).json({ Error: "Error conexion bd." })
+        }
+        const listaAprendices = JSON.parse(datos)
+        //agregar a la lista de javascript
+        listaAprendices.push(datosAprendiz)
+        //escritura de archivo
+        sistemaArchivo.writeFile(rutaArchivojson, JSON.stringify
+            (listaAprendices, null, 2
+            ), (error) => {
+                if (error) {
+                    return res.json({ Error: "No se puede reguitrar." })
+                }
+                res.status(201).json(datosAprendiz)
+        })
+    })
+})
+
+//endpoint para modificar aprendices
+app.put  ("/api/aprendices/:di", (req, res) => {
+    const diAprendiz = req.params.di
+    const datosAprendiz = req.body 
+    sistemaArchivo.readFile(rutaArchivojson, "utf-8", (error, datos) => {
+        if (error){
+            return res.status(500).json({Error:  "Error de conexion"})
+        }
+        let listaAprendices = JSON .parse(datos)
+           // actualizar aprendi
+        listaAprendices = listaAprendices.map (aprendiz => {
+            return aprendiz.di === diAprendiz ? {...aprendiz, ...datosAprendiz}:
+            aprendiz
+        })
+        //escritura de archivo
+        sistemaArchivo.writeFile(rutaArchivojson, JSON.stringify
+            (listaAprendices, null, 2), (error) =>{
+                if (error) {
+                    return res.json({Error:  "No se puede editar."})
+                }
+            res.status(200).json(datosAprendiz)
+        })
+
+        })
+    })
+
+
+
+app.listen(PORT, () => {
     console.log(`SERVIDOR http://localhost:${PORT}
         http://127.0.0.1:${PORT}`);
 });
